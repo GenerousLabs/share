6ca183f18e587eaa34384f2ebc999fd7c53e95d3
Callum Macdonald <git@callum-macdonald.com>
2020-11-17 19:05:43 +0100

    Patch the file reader.

--- expo/App.tsx
+++ expo/App.tsx
@@ -6,6 +6,8 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import './utils/patch-FileReader';
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
