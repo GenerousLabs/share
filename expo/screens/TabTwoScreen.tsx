b39d53d11b923862067793cefb2e7987a4d0275e
Callum Macdonald <git@callum-macdonald.com>
2020-11-17 16:01:38 +0100

    Prettier.

--- expo/screens/TabTwoScreen.tsx
+++ expo/screens/TabTwoScreen.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
