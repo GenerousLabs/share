4d7a26f5aca65d157fda5aaf67bfd62904afcb93
Callum Macdonald <git@callum-macdonald.com>
2020-11-17 16:01:28 +0100

    Prettier.

--- expo/screens/TabTwoScreen.tsx
+++ expo/screens/TabTwoScreen.tsx
@@ -17,16 +17,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
