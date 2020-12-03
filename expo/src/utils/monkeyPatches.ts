e58645d9a4fb33ea1d620eee239611647ddf4710
Callum Macdonald <git@callum-macdonald.com>
2020-12-03 12:55:26 +0100

    Set some env vars. Hacky as hell.

--- expo/src/utils/monkeyPatches.ts
+++ expo/src/utils/monkeyPatches.ts
@@ -1,3 +1,14 @@
+/**
+ * The `debug` library reads `process.env.DEBUG` for its settings, there's no
+ * other easy way to set debugging flags across all nested packages
+ * unfortunately.
+ */
+// TODO Remove this for production somehow
+process.env = {
+  NODE_ENV: "development",
+  DEBUG: "*",
+};
+
 import "./patch-FileReader";
 import "node-libs-react-native/globals";
 // import { Buffer } from "buffer";
