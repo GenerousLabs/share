ba77aeb17c4c1de9f257b2c156ec35745dbc505a
Callum Macdonald <git@callum-macdonald.com>
2020-12-04 13:13:34 +0100

    Add namespaces to logger.

--- expo/src/services/log/log.service.ts
+++ expo/src/services/log/log.service.ts
@@ -9,7 +9,8 @@ import utcPlugin from "dayjs/plugin/utc";
 import * as FileSystem from "expo-file-system";
 import { join } from "../fs/fs.service";
 
-const LINE_PART_SEPARATOR = " ";
+const LINE_PART_SEPARATOR = " " as const;
+const NAMESPACE_DELIMITER = ":" as const;
 
 const logLevels = ["debug", "info", "warn", "error"] as const;
 export type LogLevel = typeof logLevels[number];
@@ -19,7 +20,10 @@ const logLevelMap = Object.fromEntries(
 
 type LogFunction = (level: LogLevel, message: string, meta?: any) => void;
 type LogLevelFunction = (message: string, meta?: any) => void;
-type Log = LogFunction & Record<LogLevel, LogLevelFunction>;
+type LogBase = LogFunction & Record<LogLevel, LogLevelFunction>;
+type Log = LogBase & {
+  extend: (namespace: string) => LogBase;
+};
 
 const dayjsUtc = dayjsBase.extend(utcPlugin);
 
@@ -71,14 +75,19 @@ export const buildLogLine = ({
   date,
   level,
   message,
+  namespace,
   meta,
 }: {
   date: string;
   level: LogLevel;
   message: string;
+  namespace: string;
   meta?: any;
 }) => {
-  const lineBase = [date, level, message];
+  const dateLevel = [date, level];
+  const withNamespace =
+    namespace.length > 0 ? dateLevel.concat(namespace) : dateLevel;
+  const lineBase = withNamespace.concat(message);
   if (typeof meta === "undefined") {
     return lineBase.join(LINE_PART_SEPARATOR);
   }
@@ -94,7 +103,11 @@ export const _getFileName = (dayjs: Dayjs) => {
   return `${dayjs.format("YYYY-MM-DD")}.log`;
 };
 
-export const _log: LogFunction = (level, message, meta) => {
+export const _logFunctionFactory = (namespace: string): LogFunction => (
+  level,
+  message,
+  meta
+) => {
   const dayjs = dayjsUtc.utc();
   const date = _getDateString(dayjs);
   const filename = _getFileName(dayjs);
@@ -102,6 +115,7 @@ export const _log: LogFunction = (level, message, meta) => {
     date,
     level,
     message,
+    namespace,
     meta,
   });
   // NOTE: We don't await here, we want `_log()` to return instantly
@@ -110,13 +124,18 @@ export const _log: LogFunction = (level, message, meta) => {
   });
 };
 
-export const log = ((): Log => {
-  const logFn = _log as Log;
+export const _logFactory = (namespaces: string[] = []): Log => {
+  const namespace = namespaces.join(NAMESPACE_DELIMITER);
+  const logFn = _logFunctionFactory(namespace) as Log;
   logLevels.forEach((level) => {
-    logFn[level] = _log.bind(null, level);
+    logFn[level] = logFn.bind(null, level);
   });
+  logFn.extend = (newNamespace: string) =>
+    _logFactory(namespaces.concat(newNamespace));
   return logFn;
-})();
+};
+
+export const log = _logFactory();
 
 export const getLogs = async ({ skipDays = 0 }: { skipDays?: number } = {}) => {
   const dayjs = dayjsUtc.utc().subtract(skipDays, "day");
