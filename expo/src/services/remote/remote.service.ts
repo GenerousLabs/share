6c79eabce4cb0d373161a62e7fd96043d46af6a0
Callum Macdonald <git@callum-macdonald.com>
2020-12-09 11:37:49 +0100

    Add a new remote URL creation helper.

--- expo/src/services/remote/remote.service.ts
+++ expo/src/services/remote/remote.service.ts
@@ -6,6 +6,24 @@ import { RepoType } from "../../shared.constants";
 import { RepoInRedux } from "../../shared.types";
 import { getConfigFromFilesystem } from "../config/config.service";
 
+export const createRemoteUrl = ({
+  protocol,
+  token,
+  host,
+  username,
+  repoName,
+}: {
+  protocol: string;
+  token: string;
+  host: string;
+  username: string;
+  repoName: string;
+}) => {
+  return {
+    url: `${protocol}://u:${token}@${host}/${username}/${repoName}.git`,
+  };
+};
+
 /**
  * In theory, this function could call an API to request a new repo, be given a
  * new token to authenticate, and so on. For now, we use one token everywhere.
@@ -28,7 +46,19 @@ export const createNewRemoteForRepo = async ({
       ? "commands"
       : uuid;
 
-  return {
-    url: `${protocol}://u:${token}@${host}/${username}/${repoName}.git`,
-  };
+  return createRemoteUrl({ protocol, token, host, username, repoName });
+};
+
+export const createRemoteUrlForSharedRepo = async ({
+  repo: { type, uuid },
+  token,
+}: {
+  repo: Pick<RepoInRedux, "type" | "uuid">;
+  token: string;
+}) => {
+  const {
+    remote: { protocol, host, username },
+  } = await getConfigFromFilesystem();
+  const repoName = uuid;
+  return createRemoteUrl({ protocol, token, host, username, repoName });
 };
