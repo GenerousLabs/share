225d1c1fc0029816ce3e9bab2d1d09c1175c0c06
Callum Macdonald <git@callum-macdonald.com>
2020-12-09 11:36:21 +0100

    Update saga creator to new spec.

--- expo/src/utils/saga.utils.ts
+++ expo/src/utils/saga.utils.ts
@@ -1,6 +1,10 @@
 import { createPromiseAction, PromiseAction } from "redux-saga-promise-actions";
-import * as effectCreators from "redux-saga-promise-actions/dist/effectCreators";
+import * as effectCreators from "redux-saga-promise-actions/effects";
+import { ForkEffect } from "redux-saga/effects";
 import { SagaGenerator } from "typed-redux-saga/macro";
+// NOTE: This is not specified as a dependency, it's a dependency of
+// `redux-saga-promise-actions` and only imported for types.
+import { TypeConstant } from "typesafe-actions";
 
 export type TakeEffectType = keyof typeof effectCreators;
 
@@ -9,8 +13,8 @@ export const createAsyncPromiseSaga = <P, R>({
   effect,
   takeType = "takeEveryPromiseAction",
 }: {
-  prefix: string;
-  effect: (action: PromiseAction<P, R>) => SagaGenerator<R>;
+  prefix: TypeConstant;
+  effect: (action: PromiseAction<TypeConstant, P, R>) => SagaGenerator<R>;
   takeType?: TakeEffectType;
 }) => {
   const actionCreators = createPromiseAction(
@@ -23,8 +27,8 @@ export const createAsyncPromiseSaga = <P, R>({
 
   const takeEffect = effectCreators[takeType];
 
-  function* saga() {
-    yield takeEffect(request.toString(), effect);
+  function* saga(): SagaGenerator<void, ForkEffect<R>> {
+    yield takeEffect(actionCreators, effect);
   }
 
   return {
