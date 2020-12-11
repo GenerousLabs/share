4dff1106d69cccc2901abab60bfa2557d2c0c012
Callum Macdonald <git@callum-macdonald.com>
2020-12-11 11:00:26 +0100

    Successful library import.

--- expo/src/scenes/Connections/scenes/Confirm/Confirm.scene.tsx
+++ expo/src/scenes/Connections/scenes/Confirm/Confirm.scene.tsx
@@ -1,9 +1,10 @@
-import React, { useMemo, useState } from "react";
-import { Share, StyleSheet, View } from "react-native";
-import { Button, Text } from "react-native-elements";
+import React, { useCallback, useMemo, useState } from "react";
+import { Alert, Share, StyleSheet, View } from "react-native";
+import { Button, Input, Text } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../../../../components/Header/Header.component";
+import { confirmConnectionSagaAction } from "../../../../services/connection/connection.saga";
 import {
   ConnectionCodeType,
   getConnectionCode,
@@ -20,6 +21,7 @@ const Confirm = ({
   connectionId: string;
   goBack: () => void;
 }) => {
+  const dispatch = useDispatch();
   const selector = useMemo(
     () => makeSelectConnectionAndRepoById(connectionId),
     [connectionId]
@@ -27,9 +29,35 @@ const Confirm = ({
   const { connection, repo } = useSelector(selector);
   const [isWorking, setIsworking] = useState(false);
 
+  const [confirmCode, setConfirmCode] = useState("");
+  const [isCodeSubmitting, setIsCodeSubmitting] = useState(false);
+
+  const submitConfirmationCode = useCallback(async () => {
+    if (confirmCode === "") {
+      Alert.alert(
+        "Error #u6oaSB",
+        `Please enter a CONFIRM_ code in the box above. If you see this error mutliple times please let us know and we'll try to fix it.`
+      );
+      return;
+    }
+    setIsCodeSubmitting(true);
+    try {
+      await dispatch(
+        confirmConnectionSagaAction({ confirmCode, connectionId })
+      );
+    } catch (error) {
+      console.error("confirmConnectionSagaAction threw #ompoRE", error);
+      Alert.alert("Error #bFRNKz", `Sorry, something went wrong. :-(`);
+      setIsCodeSubmitting(false);
+    }
+    // NOTE: The successful dispatch of the action above should result in this
+    // component being unmounted, so we don't need to unset the
+    // `isCodeSubmitting` if this resolves successfully.
+  }, [confirmCode, setIsCodeSubmitting, dispatch]);
+
   return (
     <View>
-      <Header title="Waiting" goBack={goBack} />
+      <Header title="Waiting for a reply" goBack={goBack} />
       <ScrollView>
         <View style={styles.ScrollViewInner}>
           <Text>Coming soon...</Text>
@@ -53,6 +81,22 @@ const Confirm = ({
               }}
             />
           )}
+          <Text h2>Confirm Code</Text>
+          <Text>
+            Once {connection?.name || "your friend"} sends you a confirmation
+            code, enter it here.
+          </Text>
+          <Input
+            label="Confirmation code"
+            onChangeText={setConfirmCode}
+            multiline
+            numberOfLines={12}
+          />
+          <Button
+            title="Confirm Connection"
+            loading={isCodeSubmitting}
+            onPress={submitConfirmationCode}
+          />
         </View>
       </ScrollView>
     </View>
--- expo/src/scenes/Connections/scenes/ConnectionSingle/ConnectionsSingle.scene.tsx
+++ expo/src/scenes/Connections/scenes/ConnectionSingle/ConnectionsSingle.scene.tsx
@@ -1,9 +1,9 @@
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { createSelector } from "@reduxjs/toolkit";
-import React, { useEffect, useMemo } from "react";
-import { Share, View } from "react-native";
-import { Button, Text } from "react-native-elements";
+import React, { useEffect, useMemo, useState } from "react";
+import { Alert, Share, View } from "react-native";
+import { Button, Input, Text } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
 import { useDispatch, useSelector } from "react-redux";
 import invariant from "tiny-invariant";
@@ -12,13 +12,19 @@ import { createReadAuthTokenForRepoSagaAction } from "../../../../services/comma
 import {
   ConnectionCodeType,
   createConnectionCode,
+  getConnectionCode,
+  parseSharingCode,
 } from "../../../../services/connection/connection.service";
 import {
   selectAllRepoShares,
   selectConnectionById,
 } from "../../../../services/connection/connection.state";
+import { subscribeToLibrarySagaAction } from "../../../../services/library/library.state";
 import { createRemoteUrlForSharedRepo } from "../../../../services/remote/remote.service";
-import { selectLibraryRepo } from "../../../../services/repo/repo.state";
+import {
+  selectAllRepos,
+  selectLibraryRepo,
+} from "../../../../services/repo/repo.state";
 import { ConnectionsStackParameterList } from "../../../../shared.types";
 import { RootDispatch, RootState } from "../../../../store";
 import { getKeysIfEncryptedRepo } from "../../../../utils/key.utils";
@@ -27,17 +33,20 @@ import Confirm from "../Confirm/Confirm.scene";
 const makeSelector = (connectionId: string) =>
   createSelector(
     selectAllRepoShares,
+    selectAllRepos,
     (state: RootState) => selectConnectionById(state, connectionId),
     // We select the library as a single selector to ensure that there's no race
     // conditions, either the whole package is avialable, or not.
     selectLibraryRepo,
-    (allShares, connection, library) => {
+    (allShares, repos, connection, library) => {
       const repoShare = allShares.find(
         (s) => s.connectionId === connection?.id
       );
+      const myRepo = repos.find((r) => r.id === connection?.myRepoId);
       return {
         connection,
         repoShare,
+        myRepo,
         library,
       };
     }
@@ -56,7 +65,10 @@ const ConnectionsSingle = ({
   const dispatch: RootDispatch = useDispatch();
   const { connectionId } = route.params;
   const selector = useMemo(() => makeSelector(connectionId), [connectionId]);
-  const { connection, repoShare, library } = useSelector(selector);
+  const { connection, repoShare, myRepo, library } = useSelector(selector);
+
+  const [libraryCode, setLibraryCode] = useState("");
+  const [isCodeSubmitting, setIsCodeSubmitting] = useState(false);
 
   const getOrCreateToken = useMemo(
     () => async () => {
@@ -104,6 +116,23 @@ const ConnectionsSingle = ({
       <ScrollView>
         <Text>Coming in a second or six</Text>
         <Button
+          title="Share your confirmation code"
+          onPress={async () => {
+            if (typeof myRepo === "undefined") {
+              return Alert.alert(
+                "Error #1E9uID",
+                "There was an unexpected error."
+              );
+            }
+            const code = await getConnectionCode({
+              connection,
+              repo: myRepo,
+              type: ConnectionCodeType.CONFIRM,
+            });
+            Share.share({ message: code });
+          }}
+        />
+        <Button
           title="Share your stuff code"
           onPress={async () => {
             const token = await getOrCreateToken();
@@ -122,6 +151,38 @@ const ConnectionsSingle = ({
               type: ConnectionCodeType.SHARING,
             });
             Share.share({ message: code });
+          }}
+        />
+        <Text h2>Import a library</Text>
+        <Text>
+          This is a temporary feature. We'll remove this shortly once messages
+          come online.
+        </Text>
+        <Input onChangeText={setLibraryCode} multiline numberOfLines={12} />
+        <Button
+          title="Import this library"
+          loading={isCodeSubmitting}
+          onPress={async () => {
+            if (libraryCode === "") {
+              Alert.alert(
+                "Error #noA3eQ",
+                `Please enter a code above. If this error repeats, let us know, we'll try to fix it.`
+              );
+            }
+            setIsCodeSubmitting(true);
+            const params = parseSharingCode({
+              code: libraryCode,
+              type: ConnectionCodeType.SHARING,
+            });
+            dispatch(
+              subscribeToLibrarySagaAction({
+                name: connection.name,
+                remoteUrl: params.theirRemoteUrl,
+                keysBase64: params.theirKeysBase64,
+              })
+            );
+            setLibraryCode("");
+            setIsCodeSubmitting(false);
           }}
         />
       </ScrollView>
