8f27cff7ed5d5e8641e4498ad1e8dd71503830c4
Callum Macdonald <git@callum-macdonald.com>
2021-01-26 16:34:23 +0100

    Prettier.

--- expo/src/utils/patch-FileReader.js
+++ expo/src/utils/patch-FileReader.js
@@ -3,19 +3,19 @@
 /**
  * @format
  */
-FileReader.prototype.readAsArrayBuffer = function(blob) {
-  if (this.readyState === this.LOADING) throw new Error('InvalidStateError');
+FileReader.prototype.readAsArrayBuffer = function (blob) {
+  if (this.readyState === this.LOADING) throw new Error("InvalidStateError");
   this._setReadyState(this.LOADING);
   this._result = null;
   this._error = null;
   const fr = new FileReader();
   fr.onloadend = () => {
     const content = atob(
-      fr.result.substr('data:application/octet-stream;base64,'.length),
+      fr.result.substr("data:application/octet-stream;base64,".length)
     );
     const buffer = new ArrayBuffer(content.length);
     const view = new Uint8Array(buffer);
-    view.set(Array.from(content).map(c => c.charCodeAt(0)));
+    view.set(Array.from(content).map((c) => c.charCodeAt(0)));
     this._result = buffer;
     this._setReadyState(this.DONE);
   };
@@ -24,14 +24,14 @@ FileReader.prototype.readAsArrayBuffer = function(blob) {
 
 // from: https://stackoverflow.com/questions/42829838/react-native-atob-btoa-not-working-without-remote-js-debugging
 const chars =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-const atob = (input = '') => {
-  let str = input.replace(/=+$/, '');
-  let output = '';
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+const atob = (input = "") => {
+  let str = input.replace(/=+$/, "");
+  let output = "";
 
   if (str.length % 4 == 1) {
     throw new Error(
-      "'atob' failed: The string to be decoded is not correctly encoded.",
+      "'atob' failed: The string to be decoded is not correctly encoded."
     );
   }
   for (
