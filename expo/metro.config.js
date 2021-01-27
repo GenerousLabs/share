0d16d4f4e376d289453144e1e7fbd57ae4a62edc
Callum Macdonald <git@callum-macdonald.com>
2021-01-27 16:05:10 +0100

    Move metro config into `metro.config.js`.
    Consequence of upgrading to expo SDK 40.

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -1,5 +1,10 @@
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
 module.exports = {
   resolver: {
     extraNodeModules: require("node-libs-react-native"),
+    sourceExts: [...defaultConfig.resolver.sourceExts, "cjs"],
   },
 };
