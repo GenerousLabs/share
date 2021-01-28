c499025316be4e4a8819fa9b067ba6ec144ad7d1
Callum Macdonald <git@callum-macdonald.com>
2021-01-28 18:35:12 +0100

    Stop stubbing the `crypto` module.
    https://github.com/parshap/node-libs-react-native/issues/23#issuecomment-569808488

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -1,10 +1,15 @@
 const { getDefaultConfig } = require("@expo/metro-config");
+const nodelibs = require("node-libs-react-native");
+
+// Do not shim the crypto module
+// https://github.com/parshap/node-libs-react-native/issues/23#issuecomment-569808488
+nodelibs.crypto = null;
 
 const defaultConfig = getDefaultConfig(__dirname);
 
 module.exports = {
   resolver: {
-    extraNodeModules: require("node-libs-react-native"),
+    extraNodeModules: nodelibs,
     sourceExts: [...defaultConfig.resolver.sourceExts, "cjs"],
   },
 };
