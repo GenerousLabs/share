3a308e0dd95a6cdd2e5e9bd279dc945dec9c35f6
Callum Macdonald <git@callum-macdonald.com>
2021-01-30 15:51:40 +0100

    Add a rehydrate timeout on dev.

--- expo/src/services/store/store.config.ts
+++ expo/src/services/store/store.config.ts
@@ -3,6 +3,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 // NOTE: We put this into a separate file as we need to require it from multiple
 // places, and this avoids having a circular depenency.
 export const persistConfig = {
+  // Add a 10s timeout to help with slow starts while remotely debugging JS
+  timeout: __DEV__ ? 10e3 : 1e3,
   key: "root",
   storage: AsyncStorage,
   version: 1,
