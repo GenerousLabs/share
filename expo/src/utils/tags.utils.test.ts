e293488032798beedd08f8510702b881482601c7
Callum Macdonald <git@callum-macdonald.com>
2021-02-02 13:46:46 +0100

    Better tag parsing.

--- expo/src/utils/tags.utils.test.ts
+++ expo/src/utils/tags.utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "@jest/globals";
-import { parseTags } from "./tags.utils";
+import { hashifyTags, parseTags } from "./tags.utils";
 
 describe("tag.utils", () => {
   describe("parseTags()", () => {
@@ -9,6 +9,36 @@ describe("tag.utils", () => {
 
     it("Strips extraneous characters #pcOgbf", () => {
       expect(parseTags("#foo@#% bar")).toEqual(["foo", "bar"]);
+    });
+
+    it("Ignores leading and trailing whitespace #0fGph0", () => {
+      expect(parseTags(" foo   bar     ")).toEqual(["foo", "bar"]);
+    });
+
+    it("Returns empty array for only invalid characters #H7H3Wk", () => {
+      expect(parseTags("   , $ *  ")).toEqual([]);
+    });
+
+    it("Ignores tags made of only - or _ #nQV9o3", () => {
+      expect(parseTags(" - -- _____ -_-__ 3one")).toEqual(["3one"]);
+    });
+
+    it("Only accepts - or _ in the middle of tags #DPBYiu", () => {
+      expect(parseTags(" -- -_foo-_  bar-bar baz_baz_")).toEqual([
+        "foo",
+        "bar-bar",
+        "baz_baz",
+      ]);
+    });
+  });
+
+  describe("hashifyTags()", () => {
+    it("Returns empty string for no tags #JpTmsA", () => {
+      expect(hashifyTags([])).toEqual("");
+    });
+
+    it("Returns hashed tags #jjWF6w", () => {
+      expect(hashifyTags(["foo", "bar", "baz"])).toEqual("#foo, #bar, #baz");
     });
   });
 });
--- expo/src/utils/tags.utils.ts
+++ expo/src/utils/tags.utils.ts
@@ -1,6 +1,21 @@
+import trimChars from "lodash/fp/trimChars";
+
 const DISALLOWED_REGEX = new RegExp("[^\\w-]", "g");
 
+const trimLeadingAndTrailingDashesAndUnderscores = trimChars("-_");
+
 export const parseTags = (input: string): string[] => {
-  const tags = input.split(" ").map((tag) => tag.replace(DISALLOWED_REGEX, ""));
+  const tags = input
+    .split(" ")
+    .map((tag) => tag.replace(DISALLOWED_REGEX, ""))
+    .map(trimLeadingAndTrailingDashesAndUnderscores)
+    .filter((tag) => tag.length > 0);
   return tags;
+};
+
+export const hashifyTags = (tags: string[]): string => {
+  if (tags.length === 0) {
+    return "";
+  }
+  return `#${tags.join(", #")}`;
 };
