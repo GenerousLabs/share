836232fb88332063806556e01493a8f6e4aa4438
Callum Macdonald <git@callum-macdonald.com>
2021-02-04 16:24:42 +0100

    Skip me & command repos.

--- expo/src/services/repo/sagas/loadRepoFromFilesystem.saga.ts
+++ expo/src/services/repo/sagas/loadRepoFromFilesystem.saga.ts
@@ -1,5 +1,6 @@
-import { call, put, putResolve } from "typed-redux-saga/macro";
-import { RepoInRedux, RepoYaml } from "../../../shared.types";
+import { call, put } from "typed-redux-saga/macro";
+import { RepoType } from "../../../shared.constants";
+import { RepoYaml } from "../../../shared.types";
 import { createAsyncPromiseSaga } from "../../../utils/saga.utils";
 import { loadRepoContentsEffect } from "../repo.saga";
 import { getRepoParamsFromFilesystem, getRepoPath } from "../repo.service";
@@ -19,6 +20,11 @@ const saga = createAsyncPromiseSaga<
     const repoOnDisk = yield* call(getRepoParamsFromFilesystem, { path });
 
     yield* put(updateOneRepoAction({ id: repoYaml.id, changes: repoOnDisk }));
+
+    // The commands and me repos do not need to be loaded from disk
+    if (repoYaml.type === RepoType.commands || repoYaml.type === RepoType.me) {
+      return;
+    }
 
     yield* call(
       loadRepoContentsEffect,
