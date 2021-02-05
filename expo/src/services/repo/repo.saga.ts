6de0ff019814e1619c0a0e7023e246b433a50304
Callum Macdonald <git@callum-macdonald.com>
2021-02-05 16:04:19 +0100

    Don't load me / commands repos from disk.

--- expo/src/services/repo/repo.saga.ts
+++ expo/src/services/repo/repo.saga.ts
@@ -1,5 +1,6 @@
 import { all, putResolve, takeEvery } from "redux-saga/effects";
 import { call, put, select } from "typed-redux-saga/macro";
+import { RepoType } from "../../shared.constants";
 import { invariantSelector } from "../../utils/invariantSelector.util";
 import { getDirectoryContents } from "../fs/fs.service";
 import { loadOfferEffect } from "../library/library.saga";
@@ -39,7 +40,13 @@ export function* loadRepoContentsEffect(
     invariantSelector(selectRepoById, "Repo not found #6jBGXO"),
     repoId
   );
-  const path = getRepoPath(repo);
+
+  // The commands and me repos should not be loaded from disk
+  if (repo.type === RepoType.commands || repo.type === RepoType.me) {
+    return;
+  }
+
+  const path = yield* call(getRepoPath, repo);
 
   const { directories } = yield* call(getDirectoryContents, {
     path,
