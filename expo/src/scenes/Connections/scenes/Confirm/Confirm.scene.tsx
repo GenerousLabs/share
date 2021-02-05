9618f3b3a8a40d32c70ec2916dc5a8b2bbf77ccc
Callum Macdonald <git@callum-macdonald.com>
2021-02-05 13:57:20 +0100

    Remove unused code.

--- expo/src/scenes/Connections/scenes/Confirm/Confirm.scene.tsx
+++ expo/src/scenes/Connections/scenes/Confirm/Confirm.scene.tsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Alert, Share, StyleSheet, View } from "react-native";
 import { Button, Input, Text } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Header from "../../../../components/Header/Header.component";
-import { confirmConnectionSagaAction } from "../../../../services/connection/connection.saga";
 import { makeSelectConnectionAndRepoById } from "../../../../services/connection/connection.state";
 import { sharedStyles } from "../../../../shared.styles";
 import { log as parentLogger } from "../../Connections.log";
@@ -18,39 +17,12 @@ const Confirm = ({
   connectionId: string;
   goBack: () => void;
 }) => {
-  const dispatch = useDispatch();
   const selector = useMemo(
     () => makeSelectConnectionAndRepoById(connectionId),
     [connectionId]
   );
   const { connection, repo } = useSelector(selector);
   const [isWorking, setIsworking] = useState(false);
-
-  const [confirmCode, setConfirmCode] = useState("");
-  const [isCodeSubmitting, setIsCodeSubmitting] = useState(false);
-
-  const submitConfirmationCode = useCallback(async () => {
-    if (confirmCode === "") {
-      Alert.alert(
-        "Error #u6oaSB",
-        `Please enter a CONFIRM_ code in the box above. If you see this error mutliple times please let us know and we'll try to fix it.`
-      );
-      return;
-    }
-    setIsCodeSubmitting(true);
-    try {
-      await dispatch(
-        confirmConnectionSagaAction({ confirmCode, connectionId })
-      );
-    } catch (error) {
-      console.error("confirmConnectionSagaAction threw #ompoRE", error);
-      Alert.alert("Error #bFRNKz", `Sorry, something went wrong. :-(`);
-      setIsCodeSubmitting(false);
-    }
-    // NOTE: The successful dispatch of the action above should result in this
-    // component being unmounted, so we don't need to unset the
-    // `isCodeSubmitting` if this resolves successfully.
-  }, [confirmCode, setIsCodeSubmitting, dispatch]);
 
   if (typeof connection === "undefined" || typeof repo === "undefined") {
     return null;
