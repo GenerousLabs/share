c3cee0fbb50b2ea807ccc67ae41e77b73da4f5fe
Callum Macdonald <git@callum-macdonald.com>
2021-04-27 16:04:45 +0200

    Remove defunct comment.

--- expo/src/services/library/library.service.ts
+++ expo/src/services/library/library.service.ts
@@ -19,9 +19,6 @@ export const isOfferNotMine = negate(isOfferMine);
  * Given an offer, convert it to a markdown & frontmatter string
  */
 export const offerToString = ({ offer }: { offer: OfferOnDisk }) => {
-  // The `repoId` does not get written to the file, it is kept only in our
-  // application. It will be defined in the future by whichever repo the offer
-  // is loaded from.
   const { bodyMarkdown, ...frontmatter } = offer;
 
   const output = matter.stringify(bodyMarkdown, frontmatter);
