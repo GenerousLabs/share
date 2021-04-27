2b0eedc86cadd06769aca6b5e0c419e2362c32e6
Callum Macdonald <git@callum-macdonald.com>
2021-04-27 17:27:17 +0200

    Remove unused code.

--- expo/src/services/library/sagas/loadLibrary.saga.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncPromiseSaga } from "../../../utils/saga.utils";
-
-const saga = createAsyncPromiseSaga<{ input: string }, { output: string }>({
-  prefix: "SHARE/library/loadLibrary",
-  *effect(action) {
-    return { output: "true" };
-  },
-});
