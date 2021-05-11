783ca33f5391131887cffd2138d9abac188b0fb8
Callum Macdonald <git@callum-macdonald.com>
2021-05-11 14:16:04 +0200

    Default to the staging env file.

--- server/pushDocker.sh
+++ server/pushDocker.sh
@@ -3,8 +3,8 @@
 # Stop if any of the steps fail
 set -e
 
-# Source the file defined in ENV_FILENAME or fallback to .env
-source "${ENV_FILENAME-.env}"
+# Source the file defined in ENV_FILENAME or fallback to .env.staging
+source "${ENV_FILENAME-.env.staging}"
 
 docker tag $DOCKER_REPO_NAME:latest $DOCKER_USER_NAME/$DOCKER_REPO_NAME
 docker push $DOCKER_USER_NAME/$DOCKER_REPO_NAME:latest
