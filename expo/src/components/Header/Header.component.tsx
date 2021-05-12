896495738e5f34e952fd0d0dd9699b2efd080763
Callum Macdonald <git@callum-macdonald.com>
2021-05-12 17:48:46 +0200

    Add "hit slop" to header icons.

--- expo/src/components/Header/Header.component.tsx
+++ expo/src/components/Header/Header.component.tsx
@@ -1,7 +1,7 @@
 import { DrawerNavigationProp } from "@react-navigation/drawer";
 import { useNavigation } from "@react-navigation/native";
 import Constants from "expo-constants";
-import React from "react";
+import React, { useCallback } from "react";
 import { Header as RNEHeader } from "react-native-elements";
 import { colours } from "../../root.theme";
 import { RootDrawerParamList } from "../../shared.types";
@@ -16,24 +16,47 @@ const getBackgroundColor = () => {
   return colours.white;
 };
 
+// This expands the touchable area of the icons without making them take up more
+// space, 16px seems to stretch them to the same height as the header.
+const iconStyle = {
+  padding: 16,
+  margin: -16,
+};
+
 const Header = ({ title, goBack }: { title?: string; goBack?: () => void }) => {
   const navigation: DrawerNavigationProp<
     RootDrawerParamList,
     "Settings"
   > = useNavigation();
 
+  const goHome = useCallback(() => {
+    try {
+      navigation.navigate("Home");
+    } catch (error) {
+      // Do nothing when the user presses the home / help icon during setup?
+    }
+  }, [navigation]);
+
   const leftComponent =
     typeof goBack === "function"
       ? {
           icon: "navigate-before",
           onPress: goBack,
+          iconStyle,
         }
       : {
           icon: "menu",
           onPress: () => {
             navigation.openDrawer();
           },
+          iconStyle,
         };
+
+  const rightComponent = {
+    icon: "home",
+    onPress: goHome,
+    iconStyle,
+  };
 
   return (
     <RNEHeader
@@ -42,16 +65,7 @@ const Header = ({ title, goBack }: { title?: string; goBack?: () => void }) => {
       centerComponent={{
         text: title || Constants.manifest.name || "Generous Share",
       }}
-      rightComponent={{
-        icon: "home",
-        onPress: () => {
-          try {
-            navigation.navigate("Home");
-          } catch (error) {
-            // Do nothing when the user presses the home / help icon during setup?
-          }
-        },
-      }}
+      rightComponent={rightComponent}
       backgroundColor={getBackgroundColor()}
     />
   );
