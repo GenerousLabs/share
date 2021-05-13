ea41f212ddd7a22971fad479f9c9244c06aacd40
Callum Macdonald <git@callum-macdonald.com>
2021-05-13 11:54:22 +0200

    Archived offer utils.

--- expo/src/services/library/library.utils.ts
+++ expo/src/services/library/library.utils.ts
@@ -2,3 +2,9 @@ import { OfferInRedux } from "../../shared.types";
 
 export const isImportedOffer = (offer: OfferInRedux) =>
   offer.mine && offer.proximity > 0;
+
+export const isArchivedOffer = (offer: OfferInRedux) =>
+  typeof offer.archivedAt !== "undefined";
+
+export const isNotArchivedOffer = (offer: OfferInRedux) =>
+  typeof offer.archivedAt === "undefined";
